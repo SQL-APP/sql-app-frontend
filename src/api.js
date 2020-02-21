6deaedd073e595b8744d63189f8ffb79c0db6463
Michael Romay <amerror@gmail.com>
2020-02-21 13:33:48 -0800

    clean up

--- src/api.js
+++ src/api.js
@@ -3,6 +3,3 @@ import request from 'superagent';
 export function getGuitars(guitarID){
     return request.get(`https://rocky-cove-46033.herokuapp.com/api/guitars${guitarID}`);
 }
-
-// import request from "superagent";
-// export const GetShoes = (shoeId) => request.get(`https://rocky-basin-80195.herokuapp.com/api/shoes${ShoeId}`)
